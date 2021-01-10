32903147ed5a209f078055eb8654b9c582577619
Teddy Ferdian <tedd.ferdy@gmail.com>
2021-01-10 15:54:20 +0700

    Penggabungan

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import HeaderNonAuth from './pages/header/headerNonAuth'
 import LandingPage from '../src/pages/landingPage/landingPage'
+import BrowseFields from './pages/FieldList/BrowseFields'
+import FieldDetails from './pages/fieldDetail/fieldDetail'
+import PlayerList from './pages/playerList/playerList'
+import Bio from './pages/Profile/Bio'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <React.Fragment>
-        <LandingPage />
-      </React.Fragment>
+      <HeaderNonAuth />
+      <Switch>
+        <Route path='/' exact>
+          <LandingPage />
+        </Route>
+        <Route path='/browseFields' exact>
+          <BrowseFields />
+        </Route>
+        <Route path='/field-details' exact>
+          <FieldDetails />
+        </Route>
+        <Route path='/player-list' exact>
+          <PlayerList />
+        </Route>
+        <Route path='/bio' exact>
+          <Bio />
+        </Route>
+      </Switch>
     </>
   )
 }
