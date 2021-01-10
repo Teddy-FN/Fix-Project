923e316d67c4e737d6a626e3d7d977bcba8e0a9a
denisaprmastuti <denisapramastuti@gmail.com>
2021-01-10 11:53:54 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import LandingPage from '../src/pages/landingPage/landingPage'
 
@@ -10,31 +9,6 @@ function App() {
       </React.Fragment>
     </>
   )
-=======
-import './App.css';
-import FieldDetail from './component/fieldDetail/fieldDetail';
-import PlayerList from './component/playerList/playerList'
-import { Route, Switch } from "react-router-dom";
-
-
-function App() {
-  return (
-    // <div className="App">
-    //   {/* <FieldDetail /> */}
-    //   <PlayerList />
-    // </div>
-    <>
-      <Switch>
-        <Route path='/' exact>
-          <FieldDetail />
-        </Route>
-        <Route path='/player-list' exact>
-          <PlayerList />
-        </Route>
-      </Switch>
-    </>
-  );
->>>>>>> 1a7f451dd2706021e1b1630292415cd7712a65ad
 }
 
 export default App;
